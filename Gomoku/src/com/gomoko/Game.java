a823371687910797dfee92d5780279213c486e03
wangchuanli <wangchuanli_@hotmail.com>
2018-12-03 11:23:40 +0800

    java-五子棋项目

---
+++ Gomoku/src/com/gomoko/Game.java
@@ -0,0 +1,12 @@
+package com.gomoko;
+
+import javax.swing.JFrame;
+
+public class Game {
+	public static void main(String[] args) {
+        GameFrame game = new GameFrame();
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.show();
+    }
+}
+
