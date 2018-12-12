27ee75bed3debf635bdb68bdb01093bf6ff62703
wangchuanli <wangchuanli_@hotmail.com>
2018-12-12 11:18:34 +0800

    品优购商品页服务

---
+++ pinyougou-parent/pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/PageDeleteListener.java
@@ -0,0 +1,38 @@
+package com.pinyougou.page.service.impl;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.TextMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.pinyougou.page.service.ItemPageService;
+@Component
+public class PageDeleteListener implements MessageListener {
+
+	@Autowired
+	private ItemPageService itemPageService;
+	
+	@Override
+	public void onMessage(Message message) {
+		
+		ObjectMessage objectMessage  =(ObjectMessage)message;
+		try {
+			Long [] goodsIds= (Long[]) objectMessage.getObject();
+			System.out.println("接收到消息:"+goodsIds);
+			boolean b = itemPageService.deleteItemHtml(goodsIds);		
+			System.out.println("删除网页："+b);
+			
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
