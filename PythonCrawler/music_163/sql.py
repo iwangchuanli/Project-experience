b185268d2f5c99a030e1f17247a11e67975a8151
wangchuanli <wangchuanli_@hotmail.com>
2018-12-18 11:14:24 +0800

    update sql

--- PythonCrawler/music_163/sql.py
+++ PythonCrawler/music_163/sql.py
@@ -5,8 +5,8 @@ import pymysql.cursors
 
 connection = pymysql.connect(host='localhost',
                              user='root',
-                             password='1234',
-                             db='test',
+                             password='123456',
+                             db='musicComment',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
